ac1d78fdcd294f54b2aa97dcf09412400dcc4723
Rozak44 <62049237+rojaksultan44@users.noreply.github.com>
2020-04-21 13:06:21 +0700

    ngerubah lingkaran seng aksi 3

--- script.js
+++ script.js
@@ -123,24 +123,13 @@ document.addEventListener('DOMContentLoaded', ()=>{
     //Mata Kiri
     ctx.save();
     ctx.beginPath();
-    ctx.translate(125, 585)
+    ctx.translate(150, 610)
     ctx.scale(1,1)
-    ctx.arc(0, 0, 15, 0, Math.PI*2);
+    ctx.arc(0, 0, 35, 0, Math.PI*2);
     ctx.fillStyle = 'orange';
     ctx.fill();
     ctx.closePath();
-    ctx.restore();
-
-    //Mata Kanan
-    ctx.save();
-    ctx.beginPath();
-    ctx.translate(175, 585)
-    ctx.scale(1,1)
-    ctx.arc(0, 0, 15, 0, Math.PI*2);
-    ctx.fillStyle = 'orange';
-    ctx.fill();
-    ctx.closePath();
-    ctx.restore();  
+    ctx.restore(); 
     
 //Aksi4
     //Badan
